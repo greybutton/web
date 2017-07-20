b37ed48a77f949e902a3f6b10d683d1e5cc9f5b6
greybutton <greybutton@yandex.com>
2017-07-20 13:36:09 +0900

    fix lint errors

--- server/__test__/api/v1/sectors/order.spec.js
+++ server/__test__/api/v1/sectors/order.spec.js
@@ -30,18 +30,18 @@ describe(`Sector ${apiSectors}`, () => {
       score: 1,
       desirableScore: 2,
     };
-    request(app).post(apiSectors).send(sectorFirst).then((res) => {});
+    request(app).post(apiSectors).send(sectorFirst).then(() => {});
     return request(app)
       .post(apiSectors)
       .send(sectorSecond)
       .then((res) => {
-        const recevied = res.body.sectors;
-        return recevied;
+        const sectors = res.body.sectors;
+        return sectors;
       })
-      .then((recevied) => {
+      .then((sectors) => {
         expect.hasAssertions();
         return request(app)
-          .put(`${apiSectors}/sectorOrder/${recevied[0]._id}`)
+          .put(`${apiSectors}/sectorOrder/${sectors[0]._id}`)
           .send({ indexes: { oldIndex: 0, newIndex: 1 } })
           .then((result) => {
             const recevied = result.body.sectors;
