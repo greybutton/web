c77005ffdfa87fef9c75edb45e466f657ecfac88
greybutton <greybutton@yandex.com>
2017-08-16 15:16:29 +0900

    Add tappable
    Forget add file in prev commit

--- react-ui/src/components/EisenhowerMatrixCard.js
+++ react-ui/src/components/EisenhowerMatrixCard.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { ListGroupItem, Label, ButtonToolbar, Button, Collapse, Row, Col } from 'react-bootstrap';
+import Tappable from 'react-tappable';
 
 class EisenhowerMatrixCard extends Component {
   constructor(props) {
@@ -18,26 +19,32 @@ class EisenhowerMatrixCard extends Component {
         data-id={task._id}
         onDoubleClick={() => this.setState({ open: !this.state.open })}
       >
-        <div>
-          <Label bsStyle="default">{task.time}</Label>&nbsp;
-          {task.text}
-        </div>
-        <Collapse in={this.state.open}>
-          <ButtonToolbar style={{ marginTop: 10, marginLeft: 0 }}>
-            <Row>
-              <Col xs={7} sm={8} md={7} lg={8}>
-                <Link to={`/tasks/edit/${task._id}`}>
-                  <Button>Edit</Button>
-                </Link>
-              </Col>
-              <Col xs={5} sm={4} md={5} lg={4}>
-                <Button bsStyle="danger" onClick={() => deleteTask(task._id)}>
-                  Delete
-                </Button>
-              </Col>
-            </Row>
-          </ButtonToolbar>
-        </Collapse>
+        <Tappable
+          pressDelay="500"
+          onPress={() => this.setState({ open: !this.state.open })}
+          onMouseDown={() => false}
+        >
+          <div>
+            <Label bsStyle="default">{task.time}</Label>&nbsp;
+            {task.text}
+          </div>
+          <Collapse in={this.state.open}>
+            <ButtonToolbar style={{ marginTop: 10, marginLeft: 0 }}>
+              <Row>
+                <Col xs={7} sm={8} md={7} lg={8}>
+                  <Link to={`/tasks/edit/${task._id}`}>
+                    <Button>Edit</Button>
+                  </Link>
+                </Col>
+                <Col xs={5} sm={4} md={5} lg={4}>
+                  <Button bsStyle="danger" onClick={() => deleteTask(task._id)}>
+                    Delete
+                  </Button>
+                </Col>
+              </Row>
+            </ButtonToolbar>
+          </Collapse>
+        </Tappable>
       </ListGroupItem>
     );
   }
